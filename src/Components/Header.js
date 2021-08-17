c44a049abd10701580a61d9fad3a1c1a6b95c154
sara reed <sara.reed@previdence.com>
2021-08-17 08:30:27 -0600

    org update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <h1>Summary June 2021</h1>
         <div className="org-container">
           <div className="line"></div>
-            <h3 id="org">Tia Town</h3>
+            <h3 id="org">Quarterly Sample</h3>
           <div className="line"></div>
         </div>
     </div>
